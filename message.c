1339c1fa0cdaa501f069d355efb54cd2421f88fc
Kalesh-Singh <kaleshsingh96@gmail.com>
2019-04-20 22:53:54 -0700

    Messages library

--- message.c
+++ message.c
@@ -3,62 +3,69 @@
 //
 
 #include "message.h"
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <sys/types.h>
 #include <sys/socket.h>
-#include <unistd.h>
-#include <errno.h>
 
-// Write a message to a socket
-ssize_t write_message(int sockfd, const void *message, size_t msg_length) {
-    size_t          rem_msg_len;
-    ssize_t         writ_msg_len;
-    const char *    buffer;
 
-    buffer = message;
-    rem_msg_len = msg_length;
-    while (rem_msg_len > 0) {
-        if ((writ_msg_len = write(sockfd, buffer, rem_msg_len)) <= 0) {
-            if (errno == EINTR) {
-                writ_msg_len = 0;
-            }
-            else {
-                return -1;
-            }
-        }
-        rem_msg_len -= writ_msg_len;
-        buffer += writ_msg_len;
+#define DEFAULT_RECEIVER "ALL"
+
+/*
+ * Copies message to buffer in the following format
+ *  Msg Buffer: [msg|sender|receiver]
+ */
+void msgcpy(char *msg_buffer, struct message *msg) {
+    if (msg->receiver == NULL) {
+        msg->receiver = DEFAULT_RECEIVER;
     }
-    return msg_length;
+
+    uint32_t msg_len = strlen(msg->msg) + 1;
+    uint32_t sender_len = strlen(msg->sender) + 1;
+    uint32_t receiver_len = strlen(msg->receiver) + 1;
+
+    memcpy((void *) msg_buffer, (const void *) &msg_len, sizeof(uint32_t));
+    msg_buffer += sizeof(uint32_t);
+    memcpy((void *) msg_buffer, (const void *) msg->msg, msg_len);
+    msg_buffer += msg_len;
+    memcpy((void *) msg_buffer, (const void *) &sender_len, sizeof(uint32_t));
+    msg_buffer += sizeof(uint32_t);
+    memcpy((void *) msg_buffer, (const void *) msg->sender, sender_len);
+    msg_buffer += sender_len;
+    memcpy((void *) msg_buffer, (const void *) &receiver_len, sizeof(uint32_t));
+    msg_buffer += sizeof(uint32_t);
+    memcpy((void *) msg_buffer, (const void *) msg->receiver, receiver_len);
 }
 
-// Read a message from a socket
-ssize_t read_message(int sockfd, void *message, size_t maxlen) {
-    ssize_t n, bytes_read;
-    char c, * buffer;
-    buffer = message;
 
-    for (n = 1; n < maxlen; n++) {
-        // read a char from sockfd (file descriptior) into c
-        if ((bytes_read = read(sockfd, &c, 1)) == 1) {
-            *buffer++ = c;
-            if (c == '\n') {
-                break;
-            }
-        }
-        else if (bytes_read == 0) {
-            if (n == 1) {
-                return 0;
-            }
-            else {
-                break;
-            }
-        }
-        else {
-            if (errno == EINTR) {
-                continue;
-            }
-            return -1;
-        }
+/* Unpacks incoming message on sockfd into message struct.
+ * If return value if not positive then client disconnected.*/
+int unpack_msg(int sockfd, struct message *msg) {
+    uint32_t msg_len, sender_len, receiver_len;
+
+    int bytes_recv = recv(sockfd, (void *) &msg_len, sizeof(uint32_t), 0);
+    if (bytes_recv <= 0) {
+        return bytes_recv;
     }
-    *buffer = 0;
-    return n;
+    bytes_recv = recv(sockfd, (void *) &msg->msg, msg_len, 0);
+    if (bytes_recv <= 0) {
+        return bytes_recv;
+    }
+
+    bytes_recv = recv(sockfd, (void *) &sender_len, sizeof(uint32_t), 0);
+    if (bytes_recv <= 0) {
+        return bytes_recv;
+    }
+    bytes_recv = recv(sockfd, (void *) &msg->sender, sender_len, 0);
+    if (bytes_recv <= 0) {
+        return bytes_recv;
+    }
+
+    bytes_recv = recv(sockfd, (void *) &receiver_len, sizeof(uint32_t), 0);
+    if (bytes_recv <= 0) {
+        return bytes_recv;
+    }
+    bytes_recv = recv(sockfd, (void *) &msg->receiver, receiver_len, 0);
+    return bytes_recv;
 }
--- message.h
+++ message.h
@@ -7,8 +7,24 @@
 
 # include <unistd.h>
 
-/* Function declaration */
-ssize_t read_message(int fd, void *vptr, size_t maxlen);
-ssize_t write_message(int fc, const void *vptr, size_t maxlen);
+#define MAX_USERNAME_SIZE 31   // Including '\0'  (Only 30 useful)
+#define MAX_MSG_SIZE 1001       // Including '\0' (Only 30 useful)
+#define MAX_LINE_SIZE MAX_MSG_SIZE + (2 * MAX_USERNAME_SIZE)
+
+struct message {
+    char* msg;
+    char* sender;
+    char* receiver;
+};
+
+/*
+ * Copies message to buffer in the following format
+ *  Msg Buffer: [msg|sender|receiver]
+ */
+void msgcpy(char* msg_buffer, struct message* msg);
+
+/* Unpacks incoming message on sockfd into message struct.
+ * If return value if not positive then client disconnected.*/
+int unpack_msg(int sockfd, struct message *msg);
 
 #endif //CHATS_MESSAGE_H

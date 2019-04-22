a7b8b57ce8f816c373082332df8a423ed504159a
Kalesh-Singh <kaleshsingh96@gmail.com>
2019-04-21 22:06:31 -0700

    New thread management approach

--- server-threads.c
+++ server-threads.c
@@ -7,92 +7,75 @@
 
 char msg_buffer[MAX_LINE_SIZE];                                  // Message buffer
 struct client_conn client_conns[MAX_CLIENT_CONNS];          // Client connections
-
+pthread_mutex_t mutex;      // Mutex for message buffer
 
 pthread_mutex_t next_mutex;
 pthread_cond_t next_cond;       // Condition variable to signal next msg available.
 
 void *send_msg(void *args) {
     struct client_conn *a = (struct client_conn *) args;
-    while (1) {
-        pthread_mutex_lock(&(a->alive_mutex));        // LOCK
-        if (a->alive == 1) {
-            pthread_mutex_unlock(&(a->alive_mutex));  // UNLOCK
-            acquire_shared();
-            // Only send msg if client didn't disconnect.
-            if (strlen(msg_buffer) > 0) {
-
-                void* buffer = (void*) msg_buffer;
-                for (int i = 0; i < 3; i++) {
-                    send(a->sockfd, buffer, sizeof(uint32_t), 0);
-                    buffer += sizeof(uint32_t);
-                    send(a->sockfd, buffer, strlen((char *) buffer) + 1, 0);
-                    buffer += strlen((char *) buffer) + 1;
-                }
-
-            }
-            release_shared();
-            pthread_mutex_lock(&next_mutex);
-            pthread_cond_wait(&next_cond, &next_mutex);
-            pthread_mutex_unlock(&next_mutex);
-        } else {
-            break;
+    // Don't send empty messages
+    if (strlen(msg_buffer) > 0) {
+        void *buffer = (void *) msg_buffer;
+        for (int i = 0; i < 3; i++) {
+            send(a->sockfd, buffer, sizeof(uint32_t), 0);
+            buffer += sizeof(uint32_t);
+            send(a->sockfd, buffer, strlen((char *) buffer) + 1, 0);
+            buffer += strlen((char *) buffer) + 1;
         }
     }
-    pthread_mutex_unlock(&(a->alive_mutex));          // UNLOCK
-    pthread_mutex_destroy(&(a->alive_mutex));         // Destroy the mutex to allow reinitialization for a future client
-    Close(a->sockfd);                                 // Close the connection socket
-    printf("Closed FD %d\n", a->sockfd);
-    a->cleanedup = 1;
     return NULL;
 }
 
 void *recv_msg(void *args) {
     struct client_conn *a = (struct client_conn *) args;
-    while (1) {
-        // No need to lock as only this thread can change the value of
-        // the alive variable.
-        if (a->alive == 1) {                              // UNLOCK
-            int bytes_recv = unpack_msg(a->sockfd, (&a->msg));
-            acquire_exclusive();
 
-            if (bytes_recv > 0) {
-                printf("%s: %s\n", a->msg.sender, a->msg.msg);
-                msgcpy(msg_buffer, &a->msg);
-            } else {
-                pthread_mutex_lock(&(a->alive_mutex));    // LOCK
-                a->alive = 0;                             // Client no longer alive (i.e disconnected)
-                msg_buffer[0] = '\0';
-                printf("Client with FD %d disconnected\n", a->sockfd);
-                pthread_mutex_unlock(&(a->alive_mutex));  // UNLOCK
+    // No need to lock as only this thread can change the value of
+    // the alive variable.
+    while (a->alive == 1) {
+        int bytes_recv = unpack_msg(a->sockfd, (&a->msg));
+        pthread_mutex_lock(&mutex);
+
+        if (bytes_recv > 0) {
+            printf("%s: %s\n", a->msg.sender, a->msg.msg);
+            msgcpy(msg_buffer, &a->msg);
+
+            // Spawn off send threads if a new message is received.
+            for (int i = 0; i < MAX_CLIENT_CONNS; i++) {
+                if (client_conns[i].alive == 1) {
+                    pthread_create(&(client_conns[i].send_thread), NULL, send_msg, (void *) &client_conns[i]);
+                }
             }
 
-            pthread_mutex_lock(&next_mutex);
-            pthread_cond_broadcast(&next_cond);
-            pthread_mutex_unlock(&next_mutex);
-
-            release_exclusive();
+            // Wait for the message to be sent to everyone before
+            // allowing another receiver thread to overwrite the msg_buffer.
+            for (int i = 0; i < MAX_CLIENT_CONNS; i++) {
+                if (client_conns[i].alive == 1) {
+                    pthread_join(client_conns[i].send_thread, NULL);
+                }
+            }
         } else {
-            break;
+            a->alive = 0;       // Set alive to 0
+            printf("Client with FD %d disconnected\n", a->sockfd);
+            Close(a->sockfd);                                 // Close the connection socket
+            printf("Closed FD %d\n", a->sockfd);
         }
+
+        pthread_mutex_unlock(&mutex);
     }
     return NULL;
 }
 
 void init_client_conns() {
-    init_rw_lock();                                 // Initialize the reader writer lock
-    pthread_mutex_init(&next_mutex, NULL);
-    pthread_cond_init(&next_cond, NULL);
+    pthread_mutex_init(&mutex, NULL);   // Initialize the mutex
     for (int i = 0; i < MAX_CLIENT_CONNS; i++) {    // All connections are initially not live
         client_conns[i].alive = 0;
-        client_conns[i].cleanedup = 1;
     }
 }
 
 int next_client_conn() {
     for (int i = 0; i < MAX_CLIENT_CONNS; i++) {
-        if (client_conns[i].alive == 0
-            && client_conns[i].cleanedup == 1) {
+        if (client_conns[i].alive == 0) {
             return i;
         }
     }
@@ -104,9 +87,6 @@ int add_client_conn(int sockfd) {
     if (next >= 0 && next < MAX_CLIENT_CONNS) {
         client_conns[next].sockfd = sockfd;
         client_conns[next].alive = 1;
-        client_conns[next].cleanedup = 0;
-        pthread_mutex_init(&(client_conns[next].alive_mutex), NULL);    // Initialize alive mutex
-        pthread_create(&(client_conns[next].send_thread), NULL, send_msg, (void *) &client_conns[next]);
         pthread_create(&(client_conns[next].recv_thread), NULL, recv_msg, (void *) &client_conns[next]);
         printf("Added new client with FD %d\n", sockfd);
     }

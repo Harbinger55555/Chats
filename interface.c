a3aa72e9e3dbe8435370ffa0e31ae18d4223a021
Zaykha Kyaw San <zaykha.kyawsan@gmail.com>
2019-04-26 07:01:21 +0000

    Quit command handled.

--- interface.c
+++ interface.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "interface.h"
 
@@ -41,7 +42,7 @@ void join() {
 }
 
 void quit() {
-    
+    exit(0);
 }
 
 int is_command(char *input_buffer) {
